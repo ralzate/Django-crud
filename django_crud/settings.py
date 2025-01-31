5d9c456cc2e09d7a57f928728dcc1f8b4694028a
Alejandro Alzate <alejoalzate12@gmail.com>
2025-01-31 17:37:32 -0500

    Configuration for deploying with Vercel

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--!x(hc(lh3s!q=zwju28d@%t&c5(!mm0(r5voo+j62s5jbdf2j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
